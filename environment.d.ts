d864be216e98812d9043d862809ea3bed0415db6
Mathías Gómez <43690887+drgatooo@users.noreply.github.com>
2024-01-10 00:04:07 -0500

    chore: remove unnecesary declarations in environment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -3,9 +3,6 @@ declare global {
     interface ProcessEnv {
       readonly DISCORD_TOKEN: string;
       readonly MONGO_URI: string;
-      readonly SPOTIFY_CLIENT_ID: string;
-      readonly SPOTIFY_CLIENT_SECRET: string;
-      readonly LYRICS_COOKIE: string;
       readonly TEST_GUILD_ID: string;
     }
   }
